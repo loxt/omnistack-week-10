38d0719f0e5f8b2169e55b854dcdcbe439aebc7a
loxt <emannuelloxt@hotmail.com>
2020-01-15 16:43:45 -0400

    feat: search route

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 import Dev from '../models/Dev';
+import parseStringAsArray from '../utils/parseStringAsArray';
 
 export default {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  },
+
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
 
@@ -12,7 +19,7 @@ export default {
 
       const { login } = response.data.login;
       const { name = login, avatar_url, bio } = response.data;
-      const techsArray = techs.split(',').map(tech => tech.trim());
+      const techsArray = parseStringAsArray(techs);
 
       const location = {
         type: 'Point',
---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,27 @@
+import parseStringAsArray from '../utils/parseStringAsArray';
+import Dev from '../models/Dev';
+
+export default {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+
+    return res.json(devs);
+  },
+};
