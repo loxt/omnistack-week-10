d8b3e593b14e6813e8ba8efa0a93eae3928b98fe
loxt <emannuelloxt@hotmail.com>
2020-01-17 00:04:40 -0400

    feat: cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
@@ -15,6 +16,7 @@ mongoose
   )
   .then(null);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
