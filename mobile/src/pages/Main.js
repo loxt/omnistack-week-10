3a47fad8725fde7fa8915da95ebe8c79ac49f381
loxt <emannuelloxt@hotmail.com>
2020-01-18 06:09:23 -0400

    chore: web view navigator

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { Image, StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 260
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5
+  },
+
+  devTechs: {
+    marginTop: 5
+  }
+});
+
+function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition().then(null);
+  }, []);
+  return currentRegion ? (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -22.2985625, longitude: -53.8110354 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/29847769' }} />
+
+        <Callout
+          onPress={() => {
+            navigation.navigate('Profile', {
+              github_username: 'loxt'
+            });
+          }}
+        >
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Emannuel Loxt</Text>
+            <Text style={styles.devBio}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, sequi.
+            </Text>
+            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  ) : null;
+}
+
+export default Main;
